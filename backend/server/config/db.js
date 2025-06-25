3fed106921d84ca1fc7a3cd57a49d7a056373a27
gabca <g.crv@outlook.com>
2025-06-25 23:27:07 +0100

    Deploy: .env file instead for backend

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || "mongodb+srv://gabrielferncarvalho:ekg1e0AryLkUlokm@studenthubcluster.bmy8i76.mongodb.net/?retryWrites=true&w=majority&appName=StudentHubCluster";
-    
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error('MONGODB_URI environment variable is not defined');
+    }
+
     await mongoose.connect(uri, {
       dbName: 'StudentData',
       serverApi: {
@@ -13,7 +16,7 @@ const connectDB = async () => {
         deprecationErrors: true,
       }
     });
-    
+
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('MongoDB connection failed:', error);
